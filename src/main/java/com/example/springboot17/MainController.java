413ddba1296c74f3aff59a057fa6ac7390f6b51e
flying1monkey <jesseberliner@gmail.com>
2017-09-07 11:54:29 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot17/MainController.java
@@ -0,0 +1,25 @@
+package com.example.springboot17;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController
+{
+    @RequestMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login()
+    {
+        return "login";
+    }
+
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+}
